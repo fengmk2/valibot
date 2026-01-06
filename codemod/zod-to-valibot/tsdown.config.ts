bd75101a6e9c0a12db129d9aaaddeb14d730ffbd
MK <fengmk2@gmail.com>
2026-01-06 22:40:56 +0800

    FIXUP

--- codemod/zod-to-valibot/tsdown.config.ts
+++ codemod/zod-to-valibot/tsdown.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from '@voidzero-dev/vite-plus/lib';
 
-export default defineConfig([
-  {
-    entry: ['./src/transform/index.ts'],
-    clean: true,
-    format: ['es'],
-    minify: false,
-    dts: true,
-    outDir: './dist',
-  },
-]);
+// export default defineConfig([
+//   {
+//     entry: ['./src/transform/index.ts'],
+//     clean: true,
+//     format: ['es'],
+//     minify: false,
+//     dts: true,
+//     outDir: './dist',
+//   },
+// ]);
+
+export default defineConfig({
+  entry: ['./src/transform/index.ts'],
+  clean: true,
+  format: ['es'],
+  minify: false,
+  dts: true,
+  outDir: './dist',
+});
